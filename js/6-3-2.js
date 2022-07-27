f20349d9d5a5c9c7146bc45e9bcd513172a561c3
arshdeep.singh <arshdeep.singh@my.tccd.edu>
2022-07-27 13:52:14 -0500

    first commit

---
+++ js/6-3-2.js
@@ -0,0 +1,57 @@
+
+$(window).on('load',function(){ 
+  setTimeout(loading,2000);
+  //  $("#backimage").addClass('diappearimage');
+});
+
+
+function loading()
+{
+  // $("#loading").addClass('disappear');
+  $("#youtube-area").addClass('appear');
+}
+
+//youtube API
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var ytPlayer;
+function onYouTubeIframeAPIReady() {
+    ytPlayer = new YT.Player('youtube', {
+        videoId: 'dWUj3ahUqoo',
+        controls: 0,
+        disablekb:1,
+        playerVars: {
+            playsinline: 1,
+            autoplay:1,
+            fs:0,
+            rel: 1,
+            controls: 0,
+            modestbranding: 1, 
+            iv_load_policy: 3, 
+            start:6,
+            
+            loop:1,
+        },    
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+
+function onPlayerReady(event) {
+  event.target.mute();
+  event.target.playVideo();
+// event.target.playVideo(start(6));
+}
+
+
+
+  function onPlayerStateChange(event) {
+   if (event.data == YT.PlayerState.ENDED) {
+      event.target.playVideo();
+    }
+  }
